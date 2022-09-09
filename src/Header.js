8a7dc469c83db41256f8794305a0903486d59a94
VeerasamyM <veerabca09@gmail.com>
2022-09-10 03:20:40 +0530

    add all

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,32 @@
 import React from 'react'
 import './Header.css'
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import SearchIcon from '@material-ui/icons/Search';
 function Header() {
   return (
     <div className='header'>
-        <img src='https://pngimg.com/uploads/amazon/amazon_PNG11.png'/>
-        <div className='headersearch'>
-        <input className='header__searchInput'type='text'/></div>
-        <div className='header__nav'></div>
+        <img className='header__logo' src='https://pngimg.com/uploads/amazon/amazon_PNG11.png'/>
+        <div className='header__search'>
+        <input className='header__searchInput'type='text'/>
+        <SearchIcon className='header__searchIcon' /></div>
+        <div className='header__nav'>
+          <div className='header__option'>
+            <span className='header__OptionLineOne'>Hello Guest</span>
+            <span className='header__OptionLineTwo'>Sign</span>
+          </div>
+          <div className='header__option'>
+            <span className='header__OptionLineOne'>Returns</span>
+            <span className='header__OptionLineTwo'>&Orders</span>
+          </div>
+          <div className='header__option'>
+            <span className='header__OptionLineOne'>Your</span>
+            <span className='header__OptionLineTwo'>Prime</span>
+          </div>
+          <div className='header__optionBasket'>
+            <ShoppingCartIcon/>
+            <span className='header__optionLineTwo header__BasketCount'>0</span>
+          </div>
+        </div>
 
     </div>
   )
---
+++ src/Home.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import './Home.css';
+
+import Product from './Product';
+import SimpleImageSlider from "react-simple-image-slider";
+
+function Home() {
+
+  const images = [
+    { url: "https://m.media-amazon.com/images/I/71nqaV173zL._SX3000_.jpg" },
+    { url: "https://m.media-amazon.com/images/I/71QkSMqD5CL._SX3000_.jpg" },
+    { url: "https://m.media-amazon.com/images/I/61f720t266L._SX3000_.jpg" },
+    { url: "https://m.media-amazon.com/images/I/61AQSNQQBaL._SX3000_.jpg" },
+
+    { url: "https://m.media-amazon.com/images/I/61gzEy1KqCL._SX3000_.jpg" },
+    { url: "https://m.media-amazon.com/images/I/71cp9PVuTfL._SX3000_.jpg" },
+    { url: "https://m.media-amazon.com/images/I/51qBj3502YL._SX3000_.jpg" },
+    { url: "https://m.media-amazon.com/images/I/61nhlTOivrL._SX3000_.jpg" },
+  ];
+  return (
+    <div className='Home'>
+      <div className='home__container'>
+      <div>
+      <SimpleImageSlider
+        width={1425}
+        height={300}
+        images={images}
+        showBullets={false}
+        autoPlay={true}
+        
+        showNavs={true}
+        
+      />
+      
+    </div>
+      
+        <div className='home__row'>
+                <Product id='124545' title='Samsung Galaxy S20 FE 5G (Cloud Navy, 8GB RAM, 128GB Storage) with No Cost EMI & Additional Exchange Offers'
+                price={39990} image="https://images-eu.ssl-images-amazon.com/images/I/41ezRvTwcaL._SX300_SY300_QL70_FMwebp_.jpg"
+                 rating={3}/>
+
+                <Product id='2423423' title='Samsung Galaxy Buds Pro Intelligent ANC with 99% Noise Cancellation Bluetooth Truly Wireless in Ear Earbuds wiSamsung Galaxy Buds Pro Intelligent ANC with 99% Noise Cancellation Bluetooth Truly Wireless in Ear Earbuds with Mic Wireless Charging, 28 Hours Playtime (Silver)Samsung Galaxy Buds Pro Intelligent ANC wit…'
+                price={1500.0} image="https://images-eu.ssl-images-amazon.com/images/I/41zYFz1vfxL._AC_SX184_.jpg"
+                 rating={5} />
+                
+            </div>
+            <div className='home__row'>
+                <Product  id='343545'title='Fire-Boltt Ninja 2 Max 1.5" Full Touch Display, SpO2, Heart Rate, 20 Sports Mode '
+                price={1999.0} image="https://images-eu.ssl-images-amazon.com/images/I/41EQYr-n85L._AC_SX184_.jpg"
+                 rating={4}/>
+                <Product id='44355' title='Redgear Pro Series Wired Gamepad with Integrated Force Feedback, Illuminated ABXY Keys, Ergonomically Design, 1.8m USB Cable for PC'
+                price={699.0} image="https://m.media-amazon.com/images/I/51300qVfYmL._AC_UL320_.jpg"
+                 rating={3}/>
+                <Product id='53432' title='Redgear Shadow Blade Mechanical Keyboard with Drive Customization, Spectrum LED Lights, Media Control Knob and Wrist Support (Black)'
+                price={1999.0} image="https://m.media-amazon.com/images/I/61NKGdlO36L._AC_UL320_.jpg"
+                 rating={4}/>
+                
+            </div>
+            <div className='home__row'>
+                <Product id='634243' title='Mi 80 cm (32 inches) 5A Series HD Ready Smart Android LED TV L32M7-5AIN (Black)'
+                price={13999.00} image="https://m.media-amazon.com/images/I/81tD7OZ0RXL._AC_UY218_.jpg"
+                 rating={4}/>
+                
+            </div>
+                    
+        
+        
+      </div>
+    </div>
+  )
+}
+
+export default Home
