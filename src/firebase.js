f3165554a395b6d6c792ebd05a5c05688d86f786
VeerasamyM <veerabca09@gmail.com>
2022-09-10 02:23:07 +0530

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBt2Vvj-aiXMGkE8FVZl5I-mDoKOFJWF_s",
+    authDomain: "challenge-c09ba.firebaseapp.com",
+    projectId: "challenge-c09ba",
+    storageBucket: "challenge-c09ba.appspot.com",
+    messagingSenderId: "102392951144",
+    appId: "1:102392951144:web:714918c5e42752109226dd"
+  };
